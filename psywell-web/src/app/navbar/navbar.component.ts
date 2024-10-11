a871278a8b677b1f55399ddb6e0529fe73a378bd
Isaac <isaa.rubilar@gmail.com>
2024-10-11 14:45:06 -0300

    Avance con navbar

--- psywell-web/src/app/navbar/navbar.component.ts
+++ psywell-web/src/app/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule, RouterModule] // Importamos RouterModule para que funcionen los routerLink
 })
 export class NavbarComponent {}
